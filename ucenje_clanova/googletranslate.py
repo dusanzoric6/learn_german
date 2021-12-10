978dc8a1f06a4503130c7aa46d6b55ea5ea6f311
Dusan.Zoric <Dusan.Zoric@entaingroup.com>
2021-12-10 08:52:38 +0100

    12.10

---
+++ ucenje_clanova/googletranslate.py
@@ -0,0 +1,7 @@
+# import google_translator
+#
+# google_translator.tr
+#
+# translator = google_translator()
+# translate_text = translator.translate('Hola mundo!', lang_src='es', lang_tgt='en')
+# print(translate_text)
