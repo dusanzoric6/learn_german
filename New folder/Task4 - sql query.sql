574bec29f64e007564800942885623f336cc0bd0
Dusan.Zoric <Dusan.Zoric@entaingroup.com>
2022-05-19 15:59:50 +0200

    wien text

--- New folder/Task4 - sql query.sql
+++
@@ -1,6 +0,0 @@
-SELECT Count() AS Population, Countries.name
-FROM Users
-LEFT JOIN Countries ON Users.citizenship_country_id = Countries.Id
-GROUP BY Countries.Id
-HAVING Population > 1000
-ORDER BY Population DESC
